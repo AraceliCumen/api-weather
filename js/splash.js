371b225660b5c50c920a17761676865ae5c3e9d1
melissayauri <yaurimelissa@gmail.com>
2018-02-14 19:22:07 -0500

    incorporando maquetado

---
+++ js/splash.js
@@ -0,0 +1,11 @@
+/* funcionalidad para el splash*/
+$(document).ready(function() {
+  setTimeout(function() {
+    $('.splash').fadeOut();
+    $('.principal-container').fadeIn();
+  }, 1000);
+  setTimeout(function() {
+  }, 1000);
+
+  
+});
